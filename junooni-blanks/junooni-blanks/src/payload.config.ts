a1afad6030f8367e9c5d4af75793b34015b474e7
ddigicloud <ddigicloud@gmail.com>
2025-12-16 12:34:40 +0530

    16Dec changes

--- junooni-blanks/junooni-blanks/src/payload.config.ts
+++ junooni-blanks/junooni-blanks/src/payload.config.ts
@@ -22,6 +22,9 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+   routes: {
+    admin: '/junooni',
+  },
   admin: {
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
